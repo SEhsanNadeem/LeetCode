32ec758e7f3c65cb2a156fab64c316c64336dbf2
SEhsanNadeem <66420443+SEhsanNadeem@users.noreply.github.com>
2026-07-07 20:33:01 +0500

    Time: 2 ms (98.16%), Space: 43.4 MB (41.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for(char ch : s.toCharArray())
+    {
+        if(ch=='(' || ch == '{' || ch == '[')
+        {
+            stack.push(ch);
+        }else
+        {
+
+            if(stack.isEmpty())
+            {
+                return false;
+            }
+            char top = stack.pop();
+            if(top == '(' && ch != ')' ) {return false;}
+            if(top == '{' && ch != '}' ) {return false;}
+            if(top == '[' && ch != ']' ) {return false;}
+
+        }
+    }
+
+    return stack.isEmpty()   ;
+    }
+}
